b584336d04bbc093ce14197ce0525fa4f448bfcf
joaooab <joaoovfreitas@gmail.com>
2019-12-05 22:28:56 -0300

    create map

--- app/src/main/java/br/com/maximasistemas/inove_park_app/ui/map/MapViewModel.kt
+++ app/src/main/java/br/com/maximasistemas/inove_park_app/ui/map/MapViewModel.kt
@@ -1,10 +1,10 @@
-package br.com.maximasistemas.inove_park_app.ui.dashboard
+package br.com.maximasistemas.inove_park_app.ui.map
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class DashboardViewModel : ViewModel() {
+class MapViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
         value = "This is dashboard Fragment"
